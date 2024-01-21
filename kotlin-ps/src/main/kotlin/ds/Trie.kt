b0bd706511950142de6879a1ba1c18fefe0d6511
hansol <wkdqhrh00@naver.com>
2024-01-21 17:40:22 +0900

    Trie 자료구조 학습

--- kotlin-ps/src/main/kotlin/ds/Trie.kt
+++ kotlin-ps/src/main/kotlin/ds/Trie.kt
@@ -1,2 +1,34 @@
 package ds
 
+class TrieNode(
+    val children: MutableMap<Char, TrieNode> = mutableMapOf(),
+    var isEndOfWord: Boolean = false
+)
+
+class Trie(
+    private val root: TrieNode = TrieNode()
+) {
+    fun insert(word: String) {
+        var current = root
+        for (char in word) {
+            current = current.children.getOrPut(char) { TrieNode() }
+        }
+        current.isEndOfWord = true
+    }
+
+    fun search(word: String): Boolean {
+        var current = root
+        for (char in word) {
+            current = current.children[char] ?: return false
+        }
+        return current.isEndOfWord
+    }
+
+    fun startsWith(prefix: String): Boolean {
+        var current = root
+        for (char in prefix) {
+            current = current.children[char] ?: return false
+        }
+        return true
+    }
+}
